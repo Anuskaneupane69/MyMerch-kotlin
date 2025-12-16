e897de4a72ac46d0f559ad7551c324aab92126b4
Anuskaneupane69 <69210anuskarbs@gmail.com>
2025-12-16 14:23:48 +0545

    added frontend login and signup

---
+++ app/src/main/java/com/example/mymerch/SignUpActivity.kt
@@ -0,0 +1,149 @@
+package com.example.mymerch
+
+import android.app.Activity
+import androidx.compose.foundation.Image
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.text.input.KeyboardOptions
+//import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mymerch.ui.theme.BLUE
+
+class SignUpActivityActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SignUpBody()
+        }
+    }
+}
+
+@Composable
+fun SignUpBody() {
+
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+
+    val context= LocalContext.current
+    val activity = context as Activity
+
+    Scaffold { padding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+
+            Text(
+                text = "My Merch",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(1.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "My Merch Logo",
+                modifier = Modifier
+                    .size(120.dp)
+                    .padding(8.dp),
+                alignment = Alignment.Center
+            )
+
+
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Full Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+//                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {
+                    // Signup logic here
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Sign Up")
+            }
+
+
+            Text(buildAnnotatedString {
+                append("Already have an account? ")
+
+                withStyle(SpanStyle(color = BLUE)) {
+                    append("Sign In")
+                }
+            },
+                modifier = Modifier.clickable{
+                val intent = Intent(
+                    context,
+                    LoginActivity::class.java)
+
+                context.startActivity(intent)
+                activity.finish()
+            })
+
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSignup() {
+    SignUpBody()
+    // wow
+}
